4e1817c6374341e2e11f5b8f5a48a6a26d2e7bea
Amine Benelallam <amine.benallam@gmail.com>
2015-07-01 11:20:48 +0200

    tracers

---
+++ atl-mr/src/fr/inria/atlanmod/atl_mr/utils/TraceResolver.java
@@ -0,0 +1,41 @@
+package fr.inria.atlanmod.atl_mr.utils;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+
+abstract class TraceResolver {
+
+	/**
+	 * the resource set containing sources and targets
+	 */
+	protected ResourceSet rs;
+
+	/**
+	 * a string path to the traces location
+	 */
+	protected String tracesPath;
+
+	/**
+	 * returns the the corresponding element at location i
+	 * behaves like resolveTemp (x, index (input-pattern))
+	 * @param objectURI
+	 * @return
+	 */
+	public abstract EObject resolveSourceElement (URI objectURI, int i);
+
+	/**
+	 * resolves the first object
+	 * behaves like a normal resolve
+	 * @param objectURI
+	 * @return
+	 */
+	public EObject resolveEObject (URI objectURI) {
+		return resolveSourceElement(objectURI, 0);
+	}
+
+	/**
+	 * Cleaning the traces resource at the end of the creation
+	 */
+	public abstract boolean cleanTraces() ;
+}
---
+++ atl-mr/src/fr/inria/atlanmod/atl_mr/utils/Tracer.java
@@ -0,0 +1,43 @@
+package fr.inria.atlanmod.atl_mr.utils;
+
+
+import org.eclipse.emf.ecore.EObject;
+
+public interface Tracer {
+
+	public interface Creator {
+
+		/**
+		 * maps a source element to a list of target
+		 * @param source
+		 * @param targets
+		 * @return
+		 */
+		public boolean addMapping (String source, String [] targets);
+		/**
+		 * maps a source element to a single target element
+		 * @param source
+		 * @param target
+		 * @return
+		 */
+		public boolean addMapping (String source, String target);
+	}
+
+	public interface Resolver {
+		/**
+		 * returns the the corresponding element at location i
+		 * behaves like resolveTemp (x, outputPatternAtIndex(i))
+		 * @param objectString
+		 * @return
+		 */
+		public EObject resolve (String sourceElement, int index );
+		/**
+		 * resolves the first object
+		 * behaves like a normal resolve
+		 * @param objectString
+		 * @return
+		 */
+		public EObject resolve (String sourceElement);
+
+	}
+}
