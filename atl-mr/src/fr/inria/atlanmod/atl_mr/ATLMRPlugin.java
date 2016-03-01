29196aa934b0112c03f96d2b919ae739da1e03b5
Amine Benelallam <amine.benallam@gmail.com>
2016-03-01 13:17:04 +0100

    updating manifest info and adding plugin class

---
+++ atl-mr/src/fr/inria/atlanmod/atl_mr/ATLMRPlugin.java
@@ -0,0 +1,20 @@
+package fr.inria.atlanmod.atl_mr;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+public class ATLMRPlugin implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
