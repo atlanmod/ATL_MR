1e14e8bb272cb3eec05c759fd561f14445e698b8
Amine BENELALLAM <amine.benallam@gmail.com>
2016-06-20 18:30:29 +0200

    adding copy script to docker cluster and source

--- atl-mr/copy.sh
+++ atl-mr/copy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-CONTAINER=master:/root/dist/dist-trans/
-HOST=/home/atlanmod-17/git/hadoop-cluster-docker/hadoop-master/files/dist/dist-trans/
-docker cp ./dist/atl-mr.jar $CONTAINER
-cp -f ./dist/atl-mr.jar $HOST
+CONTAINER=master:/root/dist/dist-trans
+HOST=/home/atlanmod-17/git/hadoop-cluster-docker/hadoop-master/files/dist/dist-trans
+docker cp ./dist/atl-mr.jar $CONTAINER/
+
+docker cp ./dist/libs/atlmr/plugins/fr.inria.atlanmod.neoemf.hbase_0.1.0.atlmr.jar $CONTAINER/libs/atlmr/plugins/
+
+cp -f ./dist/atl-mr.jar $HOST/
+cp -f ./dist/libs/atlmr/plugins/fr.inria.atlanmod.neoemf.hbase_0.1.0.atlmr.jar $HOST/libs/atlmr/plugins/
